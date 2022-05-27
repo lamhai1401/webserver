99f0fae8b1cfe12877c43671edba1a451f467acc
lamhai1401 <lamthanhhai141@gmail.com>
2022-05-27 18:38:07 +0700

    add dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
+use std::env;
+
+#[actix_web::main]
+async fn main() {
+    dotenv::dotenv().ok();
     println!("Hello, world!");
+    let hai = env::var("HAI").ok().unwrap();
+    println!("{}", hai);
 }
